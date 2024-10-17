ecce14a01daaf004c5f9754313f16acb7e79d8e0
Tony <97451137+TonybynMp4@users.noreply.github.com>
2024-10-17 09:39:31 +0200

    refactor: make functions clearer

--- html/main.js
+++ html/main.js
@@ -2,89 +2,111 @@ let cameraLoop = false;
 let count = 0;
 let intervalId = null;
 
+const Photo = {
+    close: function (_) {
+        if (cameraLoop) return;
+        fetch(`https://${GetParentResourceName()}/closePhoto`, {
+            method: 'POST'
+        });
+        this.reset();
+    },
+    toggle: function (data) {
+        if (data.toggle) {
+            this.set(data);
+            document.onclick = closePhoto;
+            document.onkeydown = closePhoto;
+            return;
+        }
+        this.reset();
+    },
+    reset: function () {
+        const image = document.getElementById('image');
+        const title = document.getElementById('title');
+        const subText = document.getElementById('subText');
+
+        image.src = null;
+        title.textContent = null;
+        subText.textContent = null;
+
+        const photo = document.getElementById('photo-container');
+        photo.style.display = 'none';
+    },
+    set: function (data) {
+        const image = document.getElementById('image');
+        const title = document.getElementById('title');
+        const subText = document.getElementById('subText');
+
+        image.src = data.source;
+        title.textContent = data.title || '';
+        subText.textContent = data.subText || '';
+
+        const photo = document.getElementById('photo-container');
+        photo.style.display = 'flex';
+    }
+}
+
+const Camera = {
+    updateZoom: function (zoom) {
+        const zoomElement = document.getElementById('zoom');
+        zoomElement.textContent = `${zoom}X ZOOM`;
+    },
+    updateTime: function () {
+        const timeDisplay = document.getElementById('time');
+        count++;
+
+        const hours = Math.floor(count / 3600);
+        const minutes = Math.floor((count % 3600) / 60);
+        const seconds = count % 60;
+
+        const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+        timeDisplay.textContent = formattedTime;
+    },
+    toggle: function (toggleScreen) {
+        const cameraOverlay = document.getElementById('camera-overlay');
+        if (toggleScreen) {
+            count = 0;
+            cameraOverlay.style.display = 'block';
+            intervalId = setInterval(this.updateTime, 1000); // Update time every second
+            cameraLoop = true;
+            return;
+        }
+        cameraOverlay.style.display = 'none';
+        cameraLoop = false;
+        clearInterval(intervalId);
+    }
+}
+
+const Screen = {
+    // action: 'next' | 'previous'
+    changePhoto: function (action) {
+    },
+    // toggle: boolean, photos: Array [{source: string}]
+    toggle: function (toggle, photos) {
+
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener("message", function (event) {
-        if (event.data.message == 'camera') {
-            cameraOverlay(event.data.toggle);
-        } else if (event.data.message == 'photo') {
-            togglePhoto(event.data);
-        } else if (event.data.message == 'updateZoom') {
-            updateZoom(event.data.zoom);
+        switch (event.data.message) {
+            case 'camera':
+                Camera.toggle(event.data.toggle);
+                break;
+            case 'updateZoom':
+                Camera.updateZoom(event.data.zoom);
+                break;
+            case 'photo':
+                Photo.toggle(event.data);
+                break;
+            case 'toggleScreen':
+                Screen.toggle(event.data.toggle, event.data.photos);
+                break;
+            case 'changePhoto':
+                Screen.changePhoto(event.data.action);
+                break;
+            default:
+                break;
         }
+
     });
 });
-
-function updateZoom(zoom) {
-    const zoomElement = document.getElementById('zoom');
-    zoomElement.textContent = `${zoom}X ZOOM`;
-}
-
-function closePhoto(_) {
-    if (cameraLoop) return;
-    fetch(`https://${GetParentResourceName()}/closePhoto`, {
-        method: 'POST'
-    });
-    resetPhoto();
-}
-
-function togglePhoto(data) {
-    if (data.toggle) {
-        setPhoto(data);
-        document.onclick = closePhoto;
-        document.onkeydown = closePhoto;
-        return;
-    }
-    resetPhoto();
-}
-
-function resetPhoto() {
-    const image = document.getElementById('image');
-    const title = document.getElementById('title');
-    const subText = document.getElementById('subText');
-
-    image.src = null;
-    title.textContent = null;
-    subText.textContent = null;
-
-    const photo = document.getElementById('photo-container');
-    photo.style.display = 'none';
-}
-
-function setPhoto(data) {
-    const image = document.getElementById('image');
-    const title = document.getElementById('title');
-    const subText = document.getElementById('subText');
-
-    image.src = data.source;
-    title.textContent = data.title || '';
-    subText.textContent = data.subText || '';
-
-    const photo = document.getElementById('photo-container');
-    photo.style.display = 'flex';
-}
-
-function updateTime() {
-    const timeDisplay = document.getElementById('time');
-    count++;
-
-    const hours = Math.floor(count / 3600);
-    const minutes = Math.floor((count % 3600) / 60);
-    const seconds = count % 60;
-
-    const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    timeDisplay.textContent = formattedTime;
-}
-
-function cameraOverlay(toggle) {
-    const cameraOverlay = document.getElementById('camera-overlay');
-    if (toggle) {
-        count = 0;
-        cameraOverlay.style.display = 'block';
-        intervalId = setInterval(updateTime, 1000); // Update time every second
-        cameraLoop = true;
-        return;
-    }
-    cameraOverlay.style.display = 'none';
-    cameraLoop = false;
-    clearInterval(intervalId);
-}
