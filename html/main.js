f4a26e003df2f9a515823ab4e4e6607d238fd88f
Tony <97451137+TonybynMp4@users.noreply.github.com>
2024-10-25 06:51:52 +0200

    feat: camera screen functions

--- html/main.js
+++ html/main.js
@@ -1,92 +1,272 @@
-let cameraLoop = false;
-let count = 0;
-let intervalId = null;
-
-const Photo = {
-    close: function (_) {
-        if (cameraLoop) return;
-        fetch(`https://${GetParentResourceName()}/closePhoto`, {
-            method: 'POST'
-        });
-        this.reset();
-    },
-    toggle: function (data) {
-        if (data.toggle) {
-            this.set(data);
-            document.onclick = closePhoto;
-            document.onkeydown = closePhoto;
-            return;
-        }
-        this.reset();
-    },
-    reset: function () {
-        const image = document.getElementById('image');
-        const title = document.getElementById('title');
-        const subText = document.getElementById('subText');
-
-        image.src = null;
-        title.textContent = null;
-        subText.textContent = null;
-
-        const photo = document.getElementById('photo-container');
-        photo.style.display = 'none';
-    },
-    set: function (data) {
-        const image = document.getElementById('image');
-        const title = document.getElementById('title');
-        const subText = document.getElementById('subText');
-
-        image.src = data.source;
-        title.textContent = data.title || '';
-        subText.textContent = data.subText || '';
-
-        const photo = document.getElementById('photo-container');
-        photo.style.display = 'flex';
-    }
-}
-
-const Camera = {
-    updateZoom: function (zoom) {
-        const zoomElement = document.getElementById('zoom');
-        zoomElement.textContent = `${zoom}X ZOOM`;
-    },
-    updateTime: function () {
-        const timeDisplay = document.getElementById('time');
-        count++;
-
-        const hours = Math.floor(count / 3600);
-        const minutes = Math.floor((count % 3600) / 60);
-        const seconds = count % 60;
-
-        const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-        timeDisplay.textContent = formattedTime;
-    },
-    toggle: function (toggleScreen) {
-        const cameraOverlay = document.getElementById('camera-overlay');
-        if (toggleScreen) {
-            count = 0;
-            cameraOverlay.style.display = 'block';
-            intervalId = setInterval(this.updateTime, 1000); // Update time every second
-            cameraLoop = true;
-            return;
-        }
-        cameraOverlay.style.display = 'none';
-        cameraLoop = false;
-        clearInterval(intervalId);
-    }
-}
-
-const Screen = {
-    // action: 'next' | 'previous'
-    changePhoto: function (action) {
-    },
-    // toggle: boolean, photos: Array [{source: string}]
-    toggle: function (toggle, photos) {
-
-    }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
+    let cameraLoop = false;
+    let count = 0;
+    let intervalId = null;
+
+    function toast(message) {
+        const toast = document.createElement('div');
+        toast.classList.add('toast');
+        toast.textContent = message;
+        document.getElementById('screen').appendChild(toast);
+
+        setTimeout(() => {
+            toast.remove();
+        }, 3000);
+    }
+
+    function validationModal() {
+        return new Promise((resolve) => {
+            const modal = document.createElement('dialog');
+            modal.id = 'validationModal';
+            document.getElementById('screen').appendChild(modal);
+
+            const modalContent = `
+                <h3 style="text-align: center; margin-inline: 2rem;">
+                    Are you sure you want to delete this photo?
+                </h3>
+                <div style="display: flex; justify-content: center; gap: 3rem; margin-top:2rem;">
+                    <button class="button danger" id="confirm">Delete</button>
+                    <button class="button" id="cancel">Cancel</button>
+                </div>
+          `;
+
+            modal.innerHTML = modalContent;
+            modal.showModal();
+
+            document.getElementById('confirm').addEventListener('click', () => {
+                modal.close();
+                modal.remove();
+                resolve(true);
+            });
+
+            document.getElementById('cancel').addEventListener('click', () => {
+                modal.close();
+                modal.remove();
+                resolve(false);
+            });
+        });
+    }
+
+    const Photo = {
+        close: function (_) {
+            if (cameraLoop) return;
+            fetch(`https://${GetParentResourceName()}/closePhoto`, {
+                method: 'POST'
+            });
+            this.reset();
+        },
+        toggle: function (data) {
+            if (data.toggle) {
+                this.set(data);
+                document.onclick = this.close;
+                document.onkeydown = this.close;
+                return;
+            }
+            this.reset();
+        },
+        reset: function () {
+            const image = document.getElementById('image');
+            const title = document.getElementById('title');
+            const subText = document.getElementById('subText');
+
+            image.src = null;
+            title.textContent = null;
+            subText.textContent = null;
+
+            const photo = document.getElementById('photo-container');
+            photo.style.visibility = 'hidden';
+        },
+        set: function (data) {
+            const image = document.getElementById('image');
+            const title = document.getElementById('title');
+            const subText = document.getElementById('subText');
+
+            image.src = data.source;
+            title.textContent = data.title || '';
+            subText.textContent = data.subText || '';
+
+            const photo = document.getElementById('photo-container');
+            photo.style.visibility = 'visible';
+        }
+    }
+
+    const Camera = {
+        updateZoom: function (zoom) {
+            const zoomElement = document.getElementById('zoom');
+            zoomElement.textContent = `${zoom}X ZOOM`;
+        },
+        updateTime: function () {
+            const timeDisplay = document.getElementById('time');
+            count++;
+
+            const hours = Math.floor(count / 3600);
+            const minutes = Math.floor((count % 3600) / 60);
+            const seconds = count % 60;
+
+            const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+            timeDisplay.textContent = formattedTime;
+        },
+        toggle: function (toggleScreen) {
+            const cameraOverlay = document.getElementById('camera-overlay');
+            if (toggleScreen) {
+                count = 0;
+                cameraOverlay.style.visibility = 'visible';
+                intervalId = setInterval(this.updateTime, 1000); // Update time every second
+                cameraLoop = true;
+                return;
+            }
+            cameraOverlay.style.visibility = 'hidden';
+            cameraLoop = false;
+            clearInterval(intervalId);
+        }
+    }
+
+    const Screen = {
+        Container: document.getElementById('screen-container'),
+        Image: document.getElementById('screen-image'),
+        Counter: document.getElementById('counter'),
+        CurrentImage: 0,
+        Images: [],
+        CameraSlot: null,
+
+        // action: 'next' | 'previous' | 'set', index: number
+        changePhoto: function (action, index) {
+            if (this.Images.length == 0) {
+                this.Image.src = './assets/placeholder.svg';
+                this.Counter.innerHTML = `0 / 0`;
+                this.CurrentImage = 0;
+                return;
+            }
+
+            if (this.Images.length == 1) {
+                this.Image.src = this.Images[0];
+                this.Counter.innerHTML = `1 / 1`;
+                this.CurrentImage = 0;
+                return;
+            }
+
+            if (action === 'set') {
+                this.Image.src = this.Images[this.CurrentImage];
+                this.Counter.innerHTML = `${this.CurrentImage + 1} / ${this.Images.length}`;
+                return;
+            }
+
+            if (action === 'next') {
+                this.CurrentImage++;
+
+                if (this.CurrentImage >= this.Images.length) {
+                    this.CurrentImage = 0;
+                }
+
+                this.Image.src = this.Images[this.CurrentImage];
+                this.Counter.innerHTML = `${this.CurrentImage + 1} / ${this.Images.length}`;
+
+                return;
+            }
+
+            if (action === 'previous') {
+                this.CurrentImage--;
+
+                if (this.CurrentImage < 0) {
+                    this.CurrentImage = this.Images.length - 1;
+                }
+
+                this.Image.src = this.Images[this.CurrentImage];
+                this.Counter.innerHTML = `${this.CurrentImage + 1} / ${this.Images.length}`;
+
+                return;
+            }
+
+            console.error('Unknown action', action);
+        },
+        close: function () {
+            fetch(`https://${GetParentResourceName()}/closeScreen`, {
+                method: 'POST'
+            });
+            this.Container.style.visibility = 'hidden';
+            this.reset();
+        },
+        reset: function () {
+            this.Images = [];
+            this.CurrentImage = 0;
+            this.CameraSlot = null;
+            this.Image.src = './assets/placeholder.svg';
+        },
+        toggle: function (toggle, photos, cameraSlot) {
+            if (toggle) {
+                this.Container.style.visibility = 'visible';
+                this.Images = photos;
+                this.CameraSlot = cameraSlot;
+                this.Image.src = this.Images[0];
+                this.Counter.innerHTML = `1 / ${this.Images.length}`;
+                return;
+            }
+        }
+    }
+
+    Screen.Container.addEventListener('keydown', (event) => {
+        if (event.key === 'ArrowLeft') {
+            Screen.changePhoto('previous');
+        }
+
+        if (event.key === 'ArrowRight') {
+            Screen.changePhoto('next');
+        }
+
+        if (event.key === 'Escape') {
+            Screen.close();
+        }
+    });
+    document.getElementById('close').addEventListener('click', () => Screen.close());
+    document.getElementById('next').addEventListener('click', () => Screen.changePhoto('next'));
+    document.getElementById('previous').addEventListener('click', () => Screen.changePhoto('previous'));
+    document.getElementById('copyurl').addEventListener('click', () => {
+        fetch(`https://${GetParentResourceName()}/copyUrl`, {
+            method: 'POST',
+            body: JSON.stringify({ url: Screen.Images[Screen.CurrentImage] }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        toast('URL copied to clipboard');
+    });
+    document.getElementById('printPhoto').addEventListener('click', () => {
+        if (Screen.Images.length === 0) return;
+        if (Screen.Images[Screen.CurrentImage] === undefined) return;
+
+        fetch(`https://${GetParentResourceName()}/print`, {
+            method: 'POST',
+            body: JSON.stringify({ url: Screen.Images[Screen.CurrentImage] }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((success) => {
+            if (!success) return;
+            toast('A copy of the photo was printed!');
+        });
+    });
+    document.getElementById('delete').addEventListener('click', async () => {
+        if (Screen.Images.length === 0) return;
+        if (Screen.CameraSlot === null) return;
+        const shouldDelete = await validationModal();
+        if (!shouldDelete) return;
+
+        fetch(`https://${GetParentResourceName()}/deletePhoto`, {
+            method: 'POST',
+            body: JSON.stringify({ cameraSlot: Screen.CameraSlot, photoIndex: Screen.CurrentImage, url: Screen.Images[Screen.CurrentImage] }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((success) => {
+            if (!success) return;
+
+            Screen.Images = Screen.Images.filter((_, index) => index !== Screen.CurrentImage);
+            toast('Photo deleted!');
+            Screen.changePhoto('set', Screen.CurrentImage);
+        });
+    });
+
     window.addEventListener("message", function (event) {
         switch (event.data.message) {
             case 'camera':
@@ -101,10 +281,8 @@ document.addEventListener('DOMContentLoaded', () => {
             case 'toggleScreen':
                 Screen.toggle(event.data.toggle, event.data.photos);
                 break;
-            case 'changePhoto':
-                Screen.changePhoto(event.data.action);
-                break;
             default:
+                console.error('Unknown message received', event.data);
                 break;
         }
 
